ce785fb7c0caf9c5064d9bb7267ac3ee05801d3b
Yehor Merzlov <miozzik@gmail.com>
2020-05-04 22:30:42 +0300

    ничего нового

--- log_check.py
+++ log_check.py
@@ -6,7 +6,7 @@ def check_log(fille, lines=5):
         for check_line in log_file[-int(lines)*2::2]:
             log_time = re.search(r"<Timestamp.*>(.+)</Timestamp>", check_line).group(1)
             user_name = re.search(r"<User-Name.*>(.+)</User-Name>", check_line).group(1)
-            client_ip =  re.search(r"<Client-IP-Address.*>(.+)</Client-IP-Address>", check_line).group(1)
+            client_ip = re.search(r"<Client-IP-Address.*>(.+)</Client-IP-Address>", check_line).group(1)
             client_name = re.search(r"<Client-Friendly-Name.*>(.+)</Client-Friendly-Name>", check_line).group(1)
             print(f"Подключился {user_name} с устройства {client_name} и ip: {client_ip} в {log_time}")
 
